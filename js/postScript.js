2fc4afbb13b11dee6d85140f4b09ae5502e46167
EmptyWork <22065214+EmptyWork@users.noreply.github.com>
2020-12-12 10:52:45 +0900

    Alpha v.0.0.5 - Add Copy and rework some code.

--- js/postScript.js
+++ js/postScript.js
@@ -3,25 +3,19 @@ const post = document.querySelector('#post');
 const postCounter = document.querySelectorAll('.postcounter')[0];
 const preview = document.querySelectorAll('.preview')[0];
 let map = {}; // mapping other key to be use.
-// console.log(map);
+let images;
+
 post.addEventListener('keydown', (e) => {
     map[e.key] = true; // set mapping key to be true
-    // console.log(map);
-    if( // checking for Control and Enter
-        map['Control'] // set mapping with key 'Control' 
-        && 
-        e.key === 'Enter'
-        ) {
+    if( map['Control'] && e.key === 'Enter') { // checking for Control and Enter
         postCheck();
         workFlow('Input Menggunakan Komputer');
     }
-
-    if(e.key === 'p') {
-        workFlow();
-    }
-    charCounter(); // count the char
-
-    setTimeout(() => {document.querySelector('.raw').innerHTML = htmlInjectionIncoder(post.value);}, 200);
+    if(e.key === 'p') workFlow();
+    setTimeout(() => {
+        document.querySelector('.raw-output').innerHTML = convertValue(post.value);
+        ;charCounter()
+    }, 200);
 });
 
 post.addEventListener('keyup', (e) => {
@@ -32,6 +26,7 @@ function postCheck() {
     if(post) {
         //single line if else statement
         (preview) ? preview.innerHTML = convertValue(post.value) : console.error( 'container with the class of \'.preview\' doesn\'t exist.');
+        images = preview.querySelectorAll('image');
     } else {
         console.error('\'container with the id of \'#post\' doesn\'t exist.');
     }
@@ -39,17 +34,18 @@ function postCheck() {
 
 // Converting the value into array to be process
 function convertValue(text) {
+    // let arr = text.split(/(\[+\w+:+[\w\s]+\])/g); // only inside [\w:\w]
     let arr = text.match(/\[.*?\]|\[imgs=\]/g);
     // ([\[\{])[:\w]+(\})
     const arrStore = text.match(/\[.*?\]|\[imgs=\]/g);
     text = htmlInjectionIncoder(text);
-    console.log(arrStore);
     if(arr) {
         arr.forEach(convertCase);
         for(let i = 0; i < arrStore.length; i++) {
             text = text.split(arrStore[i]).join(arr[i]);
         }
     }  // running individual check of each item inside the array
+    // return arr;
     return arrayToString(text);
 }
 
@@ -59,6 +55,7 @@ function arrayToString(y) {
     return y.toString().replace(/,(?!,)/g, " ");
 }
 
+// remove any html tag from the input
 function htmlInjectionIncoder(y) {
     return y.replace(/(<script(\s|\S)*?<\/script>)|(<style(\s|\S)*?<\/style>)|(<!--(\s|\S)*?-->)|(<\/?(\s|\S)*?>)/g, " ");
 }
@@ -79,31 +76,38 @@ function charCounter() {
     }
 }
 
-function convertCase(item, i, arr) {;
+function convertCase(item, i, arr) {
     let url;
     let locationSlice = item.search(':');
     if(locationSlice === -1) locationSlice = item.search('=');
     let tag = item.slice(1, locationSlice);
+    tag = tag.toLowerCase();
     let x = item.slice(locationSlice+1, item.length-1);
-    
-    console.log(locationSlice);
-    if(tag === "br" || tag === "hr") {arr[i] = "<"+tag+"/>";} else {arr[i] = "<"+tag+">"+x+"</"+tag+">";}
-    if(tag === "imgs=https" || tag === "imgs=http" || tag === "imgs") {
+    if(tag === "br" || tag === "hr") {arr[i] = "<"+tag+"/>";}
+    else if(tag === "imgs=https" || tag === "imgs=http" || tag === "imgs") {
         locationSlice= item.search('='); 
         url = item.slice(locationSlice+1, item.length-1);
         tag = item.slice(1, locationSlice-1);
+        tag = tag.toLowerCase();
         
         arr[i] = "<"+tag+" class=\"preview-image\" src=\""+url+"\"/>";
     }
-    if(tag === "video=https" || tag ==="video=http" || tag === "video" ) {
+    else if(tag === "video=https" || tag ==="video=http" || tag === "video" ) {
         locationSlice= item.search('='); 
         url = item.slice(locationSlice+1, item.length-1);
         source = "<source src="+url+">";
 
         arr[i] = "<"+tag+" class=\"preview-video\" autoplay nocontrols>"+source+"Your browser does not support the video tag."+"</"+tag+">";
-        console.log(url);
+    }
+    else if(locationSlice === -1) {
+        x = item.slice(1, item.length-1);
+        arr[i] = x;
+    } else {
+        arr[i] = "<"+tag+">"+x+"</"+tag+">";
     }
 }
+
+let wi = 1;
 
 //need to be done next
 function workFlow(x) {
@@ -114,14 +118,18 @@ function workFlow(x) {
 function newLogBase(x, y) {
     let logBase = document.createElement('div');
     logBase.classList.add('lognew');
-    if(x) logBase.innerHTML = x;
+    if(x) logBase.innerHTML = "["+wi+"]: "+x;
     y.appendChild(logBase);
+    wi++;
 }
-
-let images = preview.querySelectorAll('image');
-
-console.log(preview);
 
 function biggerImage() {
     console.log(images);
+}
+
+function copyItem() {
+    let x = document.querySelector('.raw-output');
+    if(x.value) {
+        navigator.clipboard.writeText(x.value);
+    } 
 }
