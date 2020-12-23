43fad0b1cc88063cdaf889ac3832fefa8f641168
EmptyWork <22065214+EmptyWork@users.noreply.github.com>
2020-12-23 21:27:00 +0900

    Alpha v.0.2.12 - Syntax update

--- js/postScript.js
+++ js/postScript.js
@@ -10,31 +10,31 @@ let map = {};
 let log;
 
 function preLoad() {
-    let preload = document.querySelector('.preload');
-    let content = document.querySelector('.inside');
-    preload.style.marginTop = 'calc(-100vh + -300px)';
-    setTimeout(function () {
-        content.style.display = "grid";
-    }, 300);
+  let preload = document.querySelector('.preload');
+  let content = document.querySelector('.inside');
+  setTimeout(function() {preload.style.marginTop = 'calc(-100vh + -300px)';}, 2000)
+  setTimeout(function() {
+    content.style.display = "grid";
+  }, 2100);
 }
 
 post.addEventListener('keydown', (e) => {
-    map[e.key] = true;
-    if (map['Control'] && e.key === 'Enter') {
-        workFlow('is running...', 'post.addEventListener()');
-        postCheck(1);
-    }
-    if (map['Control'] && e.key === 'b') {
-        selectionModify();
-    }
-    setTimeout(() => {
-        document.querySelector('.raw-output').innerHTML = convertValue(post.value);
-        charCounter();
-    }, 200);
+  map[e.key] = true;
+  if (map['Control'] && e.key === 'Enter') {
+    workFlow('is running...', 'post.addEventListener()');
+    postCheck(1);
+  }
+  if (map['Control'] && e.key === 'b') {
+    selectionModify();
+  }
+  setTimeout(() => {
+    document.querySelector('.raw-output').innerHTML = convertValue(post.value);
+    charCounter();
+  }, 200);
 });
 
 post.addEventListener('keyup', (e) => {
-    delete map[e.key];
+  delete map[e.key];
 })
 
 /** 
@@ -45,21 +45,21 @@ post.addEventListener('keyup', (e) => {
  **/
 
 function postCheck(rlog) {
-    if (post) {
-        if (preview) {
-            if (rlog) log = rlog;
-            workFlow('running...', 'postCheck()');
-            preview.innerHTML = convertValue(post.value, 1);
-        } else {
-            workFlow('error: .preview not found', 'postCheck()');
-            notifMe('container with the class of \'.preview\' doesn\'t exist.', 801, 4000);
-            console.error('container with the class of \'.preview\' doesn\'t exist.');
-        }
+  if (post) {
+    if (preview) {
+      if (rlog) log = rlog;
+      workFlow('running...', 'postCheck()');
+      preview.innerHTML = convertValue(post.value, 1);
     } else {
-        workFlow('error: #preview not found', 'postCheck()');
-        notifMe('container with the class of \'#post\' doesn\'t exist.', 800, 4000);
-        console.error('\'container with the id of \'#post\' doesn\'t exist.');
+      workFlow('error: .preview not found', 'postCheck()');
+      notifMe('container with the class of \'.preview\' doesn\'t exist.', 801, 4000);
+      console.error('container with the class of \'.preview\' doesn\'t exist.');
     }
+  } else {
+    workFlow('error: #preview not found', 'postCheck()');
+    notifMe('container with the class of \'#post\' doesn\'t exist.', 800, 4000);
+    console.error('\'container with the id of \'#post\' doesn\'t exist.');
+  }
 }
 
 /** 
@@ -70,20 +70,20 @@ function postCheck(rlog) {
  **/
 
 function convertValue(text) {
-    if (log) workFlow('start converting text', 'convertValue()');
-    let arr = text.match(/\[.*?\]|\[imgs=\]/g);
-    const arrStore = text.match(/\[.*?\]|\[imgs=\]/g);
-    text = htmlInjectionIncoder(text);
-    if (arr) {
-        arr.forEach(convertCase);
-        for (let i = 0; i < arrStore.length; i++) {
-            text = text.split(arrStore[i]).join(arr[i]);
-        }
+  if (log) workFlow('start converting text', 'convertValue()');
+  let arr = text.match(/\[.*?\]|\[imgs=\]/g);
+  const arrStore = text.match(/\[.*?\]|\[imgs=\]/g);
+  text = htmlInjectionIncoder(text);
+  if (arr) {
+    arr.forEach(convertCase);
+    for (let i = 0; i < arrStore.length; i++) {
+      text = text.split(arrStore[i]).join(arr[i]);
     }
-    if (log) workFlow('done converting', 'convertValue()');
-    if (log) notifMe('Converting Success', null, 3000);
-    log = 0;
-    return arrayToString(text);
+  }
+  if (log) workFlow('done converting', 'convertValue()');
+  if (log) notifMe('Converting Success', null, 3000);
+  log = 0;
+  return arrayToString(text);
 }
 
 
@@ -96,7 +96,7 @@ function convertValue(text) {
  **/
 
 function arrayToString(arr) {
-    return arr.toString().replace(/,(?!,)/g, " ");
+  return arr.toString().replace(/,(?!,)/g, " ");
 }
 
 /** 
@@ -108,7 +108,7 @@ function arrayToString(arr) {
  **/
 
 function htmlInjectionIncoder(text) {
-    return text.replace("<", "&lt;").replace(">", "&gt;").replace(/(<script(\s|\S)*?<\/script>)|(<style(\s|\S)*?<\/style>)|(<!--(\s|\S)*?-->)|(<\/?(\s|\S)*?>)/g, "");
+  return text.replace("<", "&lt;").replace(">", "&gt;").replace(/(<script(\s|\S)*?<\/script>)|(<style(\s|\S)*?<\/style>)|(<!--(\s|\S)*?-->)|(<\/?(\s|\S)*?>)/g, "");
 }
 
 /** 
@@ -120,22 +120,22 @@ function htmlInjectionIncoder(text) {
  **/
 
 function charCounter(type) {
-    let maxChar = 140;
-    let arr = post.value.split("");
+  let maxChar = 140;
+  let arr = post.value.split("");
 
-    if (type) {
-        postCounter.innerHTML = arr.length;
+  if (type) {
+    postCounter.innerHTML = arr.length;
+  } else {
+    postCounter.innerHTML = maxChar - arr.length;
+    if (arr.length > maxChar) {
+      postCounter.style.backgroundColor = "#df4577";
+      post.style.color = "#b4c6d7";
+      notifMe(`Your input is larger than ${maxChar}`, 1);
     } else {
-        postCounter.innerHTML = maxChar - arr.length;
-        if (arr.length > maxChar) {
-            postCounter.style.backgroundColor = "#df4577";
-            post.style.color = "#b4c6d7";
-            notifMe(`Your input is larger than ${maxChar}`, 1);
-        } else {
-            postCounter.style.backgroundColor = "";
-            post.style.color = "";
-        }
+      postCounter.style.backgroundColor = "";
+      post.style.color = "";
     }
+  }
 }
 
 /** 
@@ -146,7 +146,7 @@ function charCounter(type) {
 
 const floatNotif = document.querySelector('#float-notif');
 let unqidStored = [],
-    status = []
+  status = []
 
 /** 
  *   Main function of the notifme
@@ -160,33 +160,33 @@ let unqidStored = [],
  **/
 
 function notifMe(message, id, to) {
-    let notifTimeout = 10000,
-        unqid;
-    if (!id) id = Math.round(Math.random() * 100 + 50);
-    if (to) notifTimeout = to;
-    id++;
-    unqid = `Mx-${id}`;
+  let notifTimeout = 10000,
+    unqid;
+  if (!id) id = Math.round(Math.random() * 100 + 50);
+  if (to) notifTimeout = to;
+  id++;
+  unqid = `Mx-${id}`;
 
-    if (unqidStored[id] != unqid) {
-        if (!status[id]) {
-            unqidStored[id] = unqid;
-            status[id] = 1;
-            newMessage(message, floatNotif, unqid);
-            setTimeout(function () {
-                deleteMessage(unqid);
-                status[id] = 0;
-            }, notifTimeout);
-        }
-    } else {
-        if (status[id] === 0) {
-            status[id] = 1;
-            newMessage(message, floatNotif, unqid);
-            setTimeout(function () {
-                deleteMessage(unqid);
-                status[id] = 0;
-            }, notifTimeout);
-        }
+  if (unqidStored[id] != unqid) {
+    if (!status[id]) {
+      unqidStored[id] = unqid;
+      status[id] = 1;
+      newMessage(message, floatNotif, unqid);
+      setTimeout(function() {
+        deleteMessage(unqid);
+        status[id] = 0;
+      }, notifTimeout);
     }
+  } else {
+    if (status[id] === 0) {
+      status[id] = 1;
+      newMessage(message, floatNotif, unqid);
+      setTimeout(function() {
+        deleteMessage(unqid);
+        status[id] = 0;
+      }, notifTimeout);
+    }
+  }
 }
 
 /** 
@@ -197,10 +197,10 @@ function notifMe(message, id, to) {
  **/
 
 function deleteMessage(id) {
-    let convertId = id.slice(3, id.length);
-    let messageToDelete = floatNotif.querySelector(`.messageid_${id}`);
-    if (messageToDelete) messageToDelete.remove();
-    status[convertId] = 0;
+  let convertId = id.slice(3, id.length);
+  let messageToDelete = floatNotif.querySelector(`.messageid_${id}`);
+  if (messageToDelete) messageToDelete.remove();
+  status[convertId] = 0;
 }
 
 /** 
@@ -213,16 +213,16 @@ function deleteMessage(id) {
  **/
 
 function newMessage(message, base, messageid) {
-    let messageBody = document.createElement('div');
-    messageBody.classList.add('float-notif_message', `messageid_${messageid}`);
-    if (message) messageBody.innerHTML =
-        `<div class="float-notif_header">${messageid}
+  let messageBody = document.createElement('div');
+  messageBody.classList.add('float-notif_message', `messageid_${messageid}`);
+  if (message) messageBody.innerHTML =
+    `<div class="float-notif_header">${messageid}
         <div class="float-notif_exit" onclick="deleteMessage('${messageid}')">
             <span></span>
             <span class="left"></span>
         </div>
     </div> ${message}`;
-    base.appendChild(messageBody);
+  base.appendChild(messageBody);
 }
 
 /** 
@@ -236,42 +236,42 @@ function newMessage(message, base, messageid) {
  **/
 
 function convertCase(item, i, arr) {
-    if (log) workFlow(`converting ${item.slice(0, 20)}...`, 'convertCase()');
-    let url, altText, altTextLoc;
-    let locationSlice = item.search('=');
-    if (locationSlice === -1) locationSlice = item.search(':');
-    let tag = item.slice(1, locationSlice);
-    tag = tag.toLowerCase();
-    let x = item.slice(locationSlice + 1, item.length - 1);
-    x = styleImplementer(x);
+  if (log) workFlow(`converting ${item.slice(0, 20)}...`, 'convertCase()');
+  let url, altText, altTextLoc;
+  let locationSlice = item.search('=');
+  if (locationSlice === -1) locationSlice = item.search(':');
+  let tag = item.slice(1, locationSlice);
+  tag = tag.toLowerCase();
+  let x = item.slice(locationSlice + 1, item.length - 1);
+  x = styleImplementer(x);
 
-    if (tag === "br" || tag === "hr") {
-        arr[i] = `"<${tag}/>"`;
-    } else if (tag === "imgs") {
-        locationSlice = item.search('=');
-        url = item.slice(locationSlice + 1, item.length - 1);
-        tag = item.slice(1, locationSlice - 1);
-        altTextLoc = url.search(':');
-        altText = url.slice(altTextLoc + 1);
-        url = url.slice(0, altTextLoc);
+  if (tag === "br" || tag === "hr") {
+    arr[i] = `"<${tag}/>"`;
+  } else if (tag === "imgs") {
+    locationSlice = item.search('=');
+    url = item.slice(locationSlice + 1, item.length - 1);
+    tag = item.slice(1, locationSlice - 1);
+    altTextLoc = url.search(':');
+    altText = url.slice(altTextLoc + 1);
+    url = url.slice(0, altTextLoc);
 
-        arr[i] = `<${tag} class="preview-image" alt="${altText}" src="${url}"/>`;
-    } else if (tag === "video") {
-        locationSlice = item.search('=');
-        url = item.slice(locationSlice + 1, item.length - 1);
-        source = `<source src="${source}">`;
+    arr[i] = `<${tag} class="preview-image" alt="${altText}" src="${url}"/>`;
+  } else if (tag === "video") {
+    locationSlice = item.search('=');
+    url = item.slice(locationSlice + 1, item.length - 1);
+    source = `<source src="${source}">`;
 
-        arr[i] = `
-        <${tag} class="preview-vide" autoplay nocontrols>
+    arr[i] = `
+        <${tag} class="preview-video" autoplay nocontrols>
             ${source}
             Your browser does not support the video tag.
         </${tag}>`;
-    } else if (locationSlice === -1 || !tag) {
-        x = item.slice(1, item.length - 1);
-        arr[i] = x;
-    } else {
-        arr[i] = `<${tag}>${x}</${tag}>`;
-    }
+  } else if (locationSlice === -1 || !tag) {
+    x = item.slice(1, item.length - 1);
+    arr[i] = x;
+  } else {
+    arr[i] = `<${tag}>${x}</${tag}>`;
+  }
 }
 
 /**
@@ -290,10 +290,10 @@ let sLocS, sLocE, sTag, sTagL, sTxt, sFull;
  **/
 
 function styleImplementer(text) {
-    let styleLocator = text.match(/\w`|\w`]|\w`\s/g);
-    if (log && styleLocator) workFlow(`found styles - ${styleLocator}`, "styleImplementer()");
-    if (styleLocator) return styleReplacer(styleLocator, text);
-    return text;
+  let styleLocator = text.match(/\w`|\w`]|\w`\s/g);
+  if (log && styleLocator) workFlow(`found styles - ${styleLocator}`, "styleImplementer()");
+  if (styleLocator) return styleReplacer(styleLocator, text);
+  return text;
 }
 
 /** 
@@ -306,36 +306,36 @@ function styleImplementer(text) {
  **/
 
 function styleReplacer(arr, text) {
-    for (let i = 0; i < arr.length;) {
-        sLocS = text.search(arr[i]);
-        if (arr[i] == arr[i + 1]) {
-            sLocE = text.match(/\w\w`/g);
-            sLocE = text.search(sLocE[0]);
-            sTxt = text.slice(sLocS + 2, sLocE + 2);
-            sFull = text.slice(sLocS, sLocE + 3);
-        } else {
-            sLocE = text.search(arr[i + 1]);
-            sTxt = text.slice(sLocS + 2, sLocE + 1);
-            sFull = text.slice(sLocS, sLocE + 2);
-        }
-        sTagL = text.search("`");
-        sTag = text.slice(sLocS, sTagL);
-
-        switch (sTag) {
-            case 'b':
-            case 'i':
-                sTxt = `<${sTag}>${sTxt}</${sTag}>`;
-                break;
-            default:
-                sTxt = sTxt;
-                break;
-        }
-
-        i += 2;
-        if (log) workFlow(`converting ${sFull}`, "styleReplacer()");
-        text = text.replace(sFull, sTxt);
+  for (let i = 0; i < arr.length;) {
+    sLocS = text.search(arr[i]);
+    if (arr[i] == arr[i + 1]) {
+      sLocE = text.match(/\w\w`/g);
+      sLocE = text.search(sLocE[0]);
+      sTxt = text.slice(sLocS + 2, sLocE + 2);
+      sFull = text.slice(sLocS, sLocE + 3);
+    } else {
+      sLocE = text.search(arr[i + 1]);
+      sTxt = text.slice(sLocS + 2, sLocE + 1);
+      sFull = text.slice(sLocS, sLocE + 2);
     }
-    return text.slice(0, text.length);
+    sTagL = text.search("`");
+    sTag = text.slice(sLocS, sTagL);
+
+    switch (sTag) {
+      case 'b':
+      case 'i':
+        sTxt = `<${sTag}>${sTxt}</${sTag}>`;
+        break;
+      default:
+        sTxt = sTxt;
+        break;
+    }
+
+    i += 2;
+    if (log) workFlow(`converting ${sFull}`, "styleReplacer()");
+    text = text.replace(sFull, sTxt);
+  }
+  return text.slice(0, text.length);
 }
 
 
@@ -345,13 +345,13 @@ function styleReplacer(arr, text) {
  **/
 
 function copyItem() {
-    let x = document.querySelector('.raw-output');
-    let cuttingText = htmlInjectionIncoder(x.value.slice(0, 20))
-    workFlow(`Copying ${cuttingText}...`, 'copyItem()');
-    notifMe(`Copying <span class="inmessage">${cuttingText}...</span>`, 2, 3000);
-    if (x.value) {
-        navigator.clipboard.writeText(x.value);
-    }
+  let x = document.querySelector('.raw-output');
+  let cuttingText = htmlInjectionIncoder(x.value.slice(0, 20))
+  workFlow(`Copying ${cuttingText}...`, 'copyItem()');
+  notifMe(`Copying <span class="inmessage">${cuttingText}...</span>`, 2, 3000);
+  if (x.value) {
+    navigator.clipboard.writeText(x.value);
+  }
 }
 
 /**
@@ -371,13 +371,13 @@ const logsArea = document.querySelector('.log-area');
  **/
 
 function workFlow(text, textid) {
-    let timeout = wi * 200;
-    if (text) setTimeout(() => {
-        newLogBase(text, logsArea, textid);
-        checkItem();
-        wi = 1;
-    }, timeout);
-    wi += 2;
+  let timeout = wi * 200;
+  if (text) setTimeout(() => {
+    newLogBase(text, logsArea, textid);
+    checkItem();
+    wi = 1;
+  }, timeout);
+  wi += 2;
 }
 
 /** 
@@ -390,15 +390,15 @@ function workFlow(text, textid) {
  **/
 
 function newLogBase(text, id, textid) {
-    let logBase = document.createElement('div');
-    logBase.classList.add('lognew');
-    if (text) logBase.innerHTML = `<span>${textid}:</span> ${text}`;
-    id.appendChild(logBase);
-    logBase.scrollBy({
-        top: 100,
-        left: 0,
-        behavior: 'smooth'
-    });
+  let logBase = document.createElement('div');
+  logBase.classList.add('lognew');
+  if (text) logBase.innerHTML = `<span>${textid}:</span> ${text}`;
+  id.appendChild(logBase);
+  logBase.scrollBy({
+    top: 100,
+    left: 0,
+    behavior: 'smooth'
+  });
 }
 
 /** 
@@ -407,13 +407,13 @@ function newLogBase(text, id, textid) {
  **/
 
 function checkItem() {
-    let logs = logsArea.querySelectorAll('div');
-    if (logs.length > 50) {
-        let needToDelete = logs.length - 50;
-        for (let i = 0; i < needToDelete; i++) {
-            logsArea.removeChild(logs[i]);
-        }
+  let logs = logsArea.querySelectorAll('div');
+  if (logs.length > 50) {
+    let needToDelete = logs.length - 50;
+    for (let i = 0; i < needToDelete; i++) {
+      logsArea.removeChild(logs[i]);
     }
+  }
 }
 
 /** 
@@ -426,11 +426,11 @@ function checkItem() {
 let sel;
 
 function selectionModify() {
-    const selC = sel = window.getSelection().toString();
-    const pVal = post.value;
-    sel = `[b:${sel}]`;
-    sel = pVal.replace(selC, sel);
-    post.value = sel;
+  const selC = sel = window.getSelection().toString();
+  const pVal = post.value;
+  sel = `[b:${sel}]`;
+  sel = pVal.replace(selC, sel);
+  post.value = sel;
 }
 
 /** 
@@ -442,9 +442,9 @@ function selectionModify() {
 
 const sId = document.querySelector('.select-text');
 
-sId.addEventListener('change', function () {
-    let value = sId.value;
-    showContainer(value);
+sId.addEventListener('change', function() {
+  let value = sId.value;
+  showContainer(value);
 });
 
 /** 
@@ -455,9 +455,9 @@ sId.addEventListener('change', function () {
  **/
 
 function showContainer(data) {
-    let storeData, currentData;
-    currentData = data;
-    if (storeData);
+  let storeData, currentData;
+  currentData = data;
+  if (storeData);
 }
 
 /** 
@@ -470,14 +470,14 @@ function showContainer(data) {
 let howToTrigger = document.querySelector('.howto-message');
 let howTo = document.querySelector('.howto-float');
 
-howToTrigger.addEventListener('mouseover', function () {
-    howTo.classList.add('animation-show');
-    howTo.classList.add('on');
+howToTrigger.addEventListener('mouseover', function() {
+  howTo.classList.add('animation-show');
+  howTo.classList.add('on');
 });
 
-howToTrigger.addEventListener('mouseout', function () {
-    howTo.classList.remove('animation-show');
-    howTo.classList.remove('on');
+howToTrigger.addEventListener('mouseout', function() {
+  howTo.classList.remove('animation-show');
+  howTo.classList.remove('on');
 });
 
 /** 
@@ -488,7 +488,7 @@ howToTrigger.addEventListener('mouseout', function () {
  **/
 
 function mLog(a) {
-    let baseStyles = ["color: #fff", "background-color: #444", "padding: 2px 4px", "border-radius: 2px"].join(';');
-    console.log('%c Manual Log', baseStyles);
-    console.log(a);
+  let baseStyles = ["color: #fff", "background-color: #444", "padding: 2px 4px", "border-radius: 2px"].join(';');
+  console.log('%c Manual Log', baseStyles);
+  console.log(a);
 }
