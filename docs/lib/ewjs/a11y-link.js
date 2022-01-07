0619be73e7565b4258ab7144ce3212c5621db32c
emptywork <22065214+EmptyWork@users.noreply.github.com>
2022-01-08 01:30:10 +0900

    Add `a11y-link.js`

---
+++ docs/lib/ewjs/a11y-link.js
@@ -0,0 +1,20 @@
+const makeA11yLinks = () => {
+  const externalLinks = document.querySelectorAll('a[target=_blank]')
+  
+  const createSpanElement = (elem = null) => {
+    if(!elem) return
+    const spanElement = document.createElement('span')
+    spanElement.className = "sr-popup"
+    spanElement.textContent = "Open a new tab"
+  
+    elem.appendChild(spanElement)
+  }
+  
+  externalLinks.forEach(link => {
+    link.style.position = 'relative'
+    if(!link.getAttribute('rel')) link.setAttribute('rel', "noopener noreferrer")
+    createSpanElement(link)
+  })
+}
+
+{makeA11yLinks()}
