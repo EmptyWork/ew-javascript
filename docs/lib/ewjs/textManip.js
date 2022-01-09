ee9c3a865c8b8d39073414ad9038391859145999
emptywork <22065214+EmptyWork@users.noreply.github.com>
2022-01-09 18:48:26 +0900

    Add `behavior` option - `onhover` & `onfocus`

--- docs/lib/ewjs/textManip.js
+++ docs/lib/ewjs/textManip.js
@@ -1,17 +1,41 @@
-const letterChanger = (id) => {
+const letterChanger = (id, behavior = false) => {
   let changes = document.querySelector(id)
   const changesOriginal = changes.textContent;
+  let isHover = false;
   let i = 0;
+
+  if(!behavior) {
+    changes.addEventListener('mouseover', () => {
+      changes.textContent = changesOriginal
+      isHover = true
+    })
+    
+    changes.addEventListener('focusin', () => {
+      changes.textContent = changesOriginal
+      isHover = true
+    })
+  
+    changes.addEventListener('mouseout', () => {
+      isHover = false
+    })
+  
+    changes.addEventListener('focusout', () => {
+      isHover = false
+    })
+  }
+
   setInterval(() => {
     let symbol = ['!', '*', '#', '@', '%', '$', '^']
     let innerText = changes.textContent.split('');
     innerText[Math.floor(Math.random() * innerText.length)] = symbol[Math.floor(Math.random() * symbol.length)]
+
     if(i == 60) {
       changes.textContent = changesOriginal
       i = 0
-    } else if(i > 5) {
+    } else if(i > 5 && !isHover) {
       changes.textContent = innerText.join('')
     }
+
     i++
   }, 50)
 }
