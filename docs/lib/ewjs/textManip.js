01f25f8ddc5abdcdb9ff62fd9db9f55f785553fb
emptywork <22065214+EmptyWork@users.noreply.github.com>
2022-01-30 20:45:59 +0900

    Update `textManip.js` converted params to obj options

--- docs/lib/ewjs/textManip.js
+++ docs/lib/ewjs/textManip.js
@@ -1,4 +1,4 @@
-const letterChanger = (id, behavior = false, debug = false) => {
+const letterChanger = ({id, behavior = false, debug = false}) => {
   let changes = document.querySelectorAll(id)
   let isHover = false
   let interval = 50
@@ -87,5 +87,7 @@ const letterChanger = (id, behavior = false, debug = false) => {
 }
 
 {
-  letterChanger(".change")
+  letterChanger({
+    id: ".change"
+  })
 }
